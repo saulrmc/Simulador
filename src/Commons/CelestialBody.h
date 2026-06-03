3a054de838a0031b03d18709abf90e755ddad64f
Saul <saulramoscu@gmail.com>
2026-06-02 19:29:12 -0500

    otra micro optimizacion, evitar ciclos de cpu innecesarios para llamar a los métodos de CelestialBody

--- src/Commons/CelestialBody.h
+++ src/Commons/CelestialBody.h
@@ -66,4 +66,142 @@ class CelestialBody {
     //para evitar que los fragmentos también se sigan dividiendo en muchísimos más
 };
 
+inline CelestialBody::CelestialBody() {
+    name = "CelestialBody";
+    velocity = Vec3(0.0f, 0.0f, 0.0f);
+    position = Vec3(0.0f, 0.0f, 0.0f);
+    force = Vec3(0.0f, 0.0f, 0.0f);
+    mass = 0.0f;
+    radius = 0.0f;
+    id = 0;
+}
+
+inline CelestialBody::CelestialBody(const std::string &name, const Vec3& position, const Vec3& velocity,
+    const double mass, const double radius) {
+    this->name = name;
+    this->position = position;
+    this->velocity = velocity;
+    this->mass = mass;
+    this->radius = radius;
+}
+
+inline CelestialBody::~CelestialBody() {
+}
+
+inline std::string CelestialBody::get_name() const {
+    return name;
+}
+
+inline void CelestialBody::set_name(const std::string &name) {
+    this->name = name;
+}
+
+inline Vec3 CelestialBody::get_velocity() const {
+    return velocity;
+}
+
+inline void CelestialBody::set_velocity(const Vec3 &velocity) {
+    this->velocity = velocity;
+}
+
+inline Vec3 CelestialBody::get_position() const {
+    return position;
+}
+
+inline void CelestialBody::set_position(const Vec3 &position) {
+    this->position = position;
+}
+
+inline Vec3 CelestialBody::get_force() const {
+    return force;
+}
+
+inline void CelestialBody::set_force(const Vec3 &force) {
+    this->force = force;
+}
+
+inline double CelestialBody::get_mass() const {
+    return mass;
+}
+
+inline void CelestialBody::set_mass(const double mass) {
+    this->mass = mass;
+}
+
+inline double CelestialBody::get_radius() const {
+    return radius;
+}
+
+inline void CelestialBody::set_radius(const double radio) {
+    this->radius = radio;
+}
+
+inline void CelestialBody::operator=(const CelestialBody &body) {
+    this->name = body.get_name();
+    this->position = body.get_position();
+    this->velocity = body.get_velocity();
+    this->force = body.get_force();
+    this->mass = body.get_mass();
+    this->radius = body.get_radius();
+
+    this->accumulatedMass = body.accumulatedMass;
+    this->accumulatedVel = body.accumulatedVel;
+}
+
+inline bool CelestialBody::operator==(const CelestialBody &body) const {
+    if (this->name == body.get_name()) return true; //esta es una posible condicion, podría ser permanente
+    //o podría cambiarla por otra, probablemente sea mejor crearlas bajo un ID...
+    return false;
+}
+
+inline void CelestialBody::minor_collision(const double body_mass) {
+    this->accumulatedMass += body_mass;
+}
+
+inline bool CelestialBody::is_it_insignificant(const double body_mass) {
+    if (100*body_mass/this->mass < 0.000001) return true;
+    //o podría ser en base a un porcentaje, por ejemplo el 0,0001% de la masa
+    return false;
+}
+
+inline int CelestialBody::get_id() const {
+    return id;
+}
+
+inline void CelestialBody::set_id(const int id) {
+    this->id = id;
+}
+
+inline int CelestialBody::get_index() const {
+    return index;
+}
+
+inline void CelestialBody::set_index(const int index) {
+    this->index = index;
+}
+
+inline double CelestialBody::get_posX() const {
+    return position.get_x();
+}
+
+inline void CelestialBody::set_posX(const double x) {
+    position.set_x(x);
+}
+
+inline double CelestialBody::get_posY() const {
+    return position.get_y();
+}
+
+inline void CelestialBody::set_posY(const double y) {
+    position.set_y(y);
+}
+
+inline double CelestialBody::get_posZ() const {
+    return position.get_z();
+}
+
+inline void CelestialBody::set_posZ(const double z) {
+    position.set_z(z);
+}
+
 #endif //SIMULADORGRAVITACIONAL_CELESTIALBODY_H
